c8346e282e93f2a284c046e2ffd0c1f387487ecb
Adam Myczkowski <mycaxd511@gmail.com>
2017-11-14 22:53:26 +0000

    TicTacToe: Initial implementation of medium level and general code cleanup.
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -24,8 +24,7 @@ namespace TicTacToe
         {
             Console.WriteLine("Do you want to play with a friend or with the computer? (1|2)");
             string gamemode = Console.ReadLine();
-            int igamemmode;
-            while (!int.TryParse(gamemode, out igamemmode) || igamemmode > 2 || igamemmode < 1)
+            while (gamemode != "2" && gamemode != "1")
             {
                 Console.WriteLine($"Incorrect input {gamemode} (1|2)");
                 gamemode = Console.ReadLine();
@@ -41,6 +40,8 @@ namespace TicTacToe
             }
             Console.ReadKey();
         }
+
+        /* ================== General methods ================== */
         static void twoPlayers()
         {
             updateBoard();
@@ -52,17 +53,7 @@ namespace TicTacToe
                     break;
             }
         }
-        static void machinePlay()
-        {
-            updateBoard();
-            while (!someoneWon)
-            {
-                if (!UserInput('X') || someoneWon)
-                    break;
-                if (!ComputerInput('O') || someoneWon)
-                    break;
-            }
-        }
+
         static void updateBoard()
         {
             Console.Clear();
@@ -74,29 +65,114 @@ namespace TicTacToe
             Console.WriteLine($"|{boardValues[6]}|{boardValues[7]}|{boardValues[8]}|");
             Console.WriteLine("-------");
         }
+
+        static string freeIndex(char[] board)
+        {
+            string freeI = null;
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i] == ' ')
+                {
+                    freeI += i + 1 + " ";
+                }
+            }
+            return freeI;
+        }
+
+        static int? getRandom()
+        {
+            int? iRandom = null;
+            if (freeIndex(boardValues) != null)
+            {
+                Random random = new Random();
+                iRandom = random.Next(0, 10);
+                while (!freeIndex(boardValues).Contains(iRandom.ToString()))
+                {
+                    iRandom = random.Next(0, 10);
+                }
+            }
+            return iRandom - 1;
+        }
+
+        static bool checkWinner(char who, char[] board)
+        {
+            if ((board[0] == who && board[1] == who && board[2] == who) ||
+            (board[3] == who && board[4] == who && board[5] == who) ||
+            (board[6] == who && board[7] == who && board[8] == who) ||
+            (board[0] == who && board[3] == who && board[6] == who) ||
+            (board[1] == who && board[4] == who && board[7] == who) ||
+            (board[2] == who && board[5] == who && board[8] == who) ||
+            (board[0] == who && board[4] == who && board[8] == who) ||
+            (board[2] == who && board[4] == who && board[6] == who))
+            {
+                Console.WriteLine($"{who} won!");
+                return true;
+            }
+            return false;
+        }
+
+        /* ================== Human user ================== */
         static bool UserInput(char who)
         {
-            if (freeIndex() != null)
+            if (freeIndex(boardValues) != null)
             {
-                Console.WriteLine($"Enter the position to put {who} in (free positions: {freeIndex()})");
+                Console.WriteLine($"Enter the position to put {who} in (free positions: {freeIndex(boardValues)})");
                 string input = Console.ReadLine();
-                while (!freeIndex().Contains(input) || input == " " || input == "")
+                while (!freeIndex(boardValues).Contains(input) || input == " " || input == "")
                 {
-                    Console.WriteLine($"Index {input} is not within the {freeIndex()}range");
+                    Console.WriteLine($"Index {input} is not within the {freeIndex(boardValues)}range");
                     input = Console.ReadLine();
                 }
                 boardValues[int.Parse(input) - 1] = who;
                 updateBoard();
-                someoneWon = checkWinner(who);
+                someoneWon = checkWinner(who, boardValues);
                 return true;
             }
             updateBoard();
             Console.WriteLine("Draw");
             return false;
         }
-        static bool ComputerInput(char who)
+
+        /* ================== AI ================== */
+        static void machinePlay()
         {
-            if (freeIndex() != null)
+            Console.WriteLine("Easy(1) Medium(2)");
+            string level = Console.ReadLine();
+            while (level != "2" && level != "1")
+            {
+                Console.WriteLine($"Incorrect input {level} (1|2)");
+                level = Console.ReadLine();
+            }
+            switch (level)
+            {
+                case "1":
+                    updateBoard();
+                    while (!someoneWon)
+                    {
+                        if (!UserInput('X') || someoneWon)
+                            break;
+                        if (!ComputerInputEasy('O') || someoneWon)
+                            break;
+                    }
+                    break;
+                case "2":
+                    while (!someoneWon)
+                    {
+                        updateBoard();
+                        if (!UserInput('X') || someoneWon)
+                            break;
+                        if (!ComputerInputMedium('O', boardValues) || someoneWon)
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        /* ========= Eeasy ========= */
+        /* Places O in random position on the grid. */
+        static bool ComputerInputEasy(char who)
+        {
+            if (freeIndex(boardValues) != null)
             {
                 int? comp;
                 if ((comp = getRandom()) != null)
@@ -104,54 +180,53 @@ namespace TicTacToe
                     boardValues[comp.Value] = who;
                 }
                 updateBoard();
-                someoneWon = checkWinner(who);
+                someoneWon = checkWinner(who, boardValues);
                 return true;
             }
             updateBoard();
             Console.WriteLine("Draw");
             return false;
         }
-        static string freeIndex()
+
+        /* ========= Eeasy ========= */
+        /* Check all empty spaces for next move of O,
+           if one of them is winning, place O in there
+           otherwise place it in any free random position. */
+        static bool ComputerInputMedium(char who, char[] copyBoard)
         {
-            string freeI = null;
-            for (int i = 0; i < boardValues.Length; i++)
+            if (freeIndex(boardValues) != null)
             {
-                if (boardValues[i] == ' ')
+                int c = -1;
+                int[] freeArray = Array.ConvertAll(freeIndex(copyBoard).Split(" ", StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s) - 1);
+                foreach (int id in freeArray)
                 {
-                    freeI += i + 1 + " ";
+                    copyBoard[id] = who;
+                    if (checkWinner(who, copyBoard))
+                    {
+                        boardValues[id] = who;
+                        c = 0;
+                        break;
+                    }
+                    copyBoard[id] = ' '; // too hacky, find better way
                 }
-            }
-            return freeI;
-        }
-        static int? getRandom()
-        {
-            int? iRandom = null;
-            if (freeIndex() != null)
-            {
-                Random random = new Random();
-                iRandom = random.Next(0, 10);
-                while (!freeIndex().Contains(iRandom.ToString()))
+                if (c == -1)
                 {
-                    iRandom = random.Next(0, 10);
+                    int? comp;
+                    if ((comp = getRandom()) != null)
+                    {
+                        boardValues[comp.Value] = who;
+                    }
                 }
-            }
-            return iRandom - 1;
-        }
-        static bool checkWinner(char who)
-        {
-            if ((boardValues[0] == who && boardValues[1] == who && boardValues[2] == who) ||
-            (boardValues[3] == who && boardValues[4] == who && boardValues[5] == who) ||
-            (boardValues[6] == who && boardValues[7] == who && boardValues[8] == who) ||
-            (boardValues[0] == who && boardValues[3] == who && boardValues[6] == who) ||
-            (boardValues[1] == who && boardValues[4] == who && boardValues[7] == who) ||
-            (boardValues[2] == who && boardValues[5] == who && boardValues[8] == who) ||
-            (boardValues[0] == who && boardValues[4] == who && boardValues[8] == who) ||
-            (boardValues[2] == who && boardValues[4] == who && boardValues[6] == who))
-            {
-                Console.WriteLine($"{who} won!");
+                updateBoard();
+                someoneWon = checkWinner(who, boardValues);
                 return true;
             }
+            updateBoard();
+            Console.WriteLine("Draw");
             return false;
         }
+
+        /* ========= Hard (MiniMax) ========= */
+        // TODO
     }
 }
