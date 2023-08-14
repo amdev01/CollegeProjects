d54ebb25a22cab2911f8dea0b6c813d98ba5225b
Adam Myczkowski <am.dev.mail01@gmail.com>
2023-08-14 12:45:02 +0100

    StringCalculator: Make code more object-oriented in preparation for unit tests.

---
+++ StringCalculator/stringCalcTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+public class StringCalculatorTests {
+    [Fact]
+    public void StringCalculatorTest() {}
+
+    [Fact]
+    [Trait("UnitTest", "UnitTest")]
+    public void CanCalculate() {}
+    
+    [Fact]
+    public void CanCalculate2() {
+        
+    }
+}
+
