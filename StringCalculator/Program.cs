5cedcc389b8b5e509c5d6d718d1adb862f144385
Adam Myczkowski <am.dev.mail01@gmail.com>
2023-08-14 13:50:40 +0100

    StringCalculator: handle equation validation in calculator class Use tuple for Calc() return values, this allows me to return a status code which indicates if operation succeed or how it failed.

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 
 
+namespace Calculator
+{
     class Program
     {
         static void Main(string[] args)
         {
             StringCalculator sc = new StringCalculator();
             Console.WriteLine($"Enter the equation you want to work out down below with the maximum of {StringCalculator._maxLen} characters in the equation (enter 'help' to display help menu)");
-            string sEquation = Console.ReadLine();
-            while (sEquation.Length > StringCalculator._maxLen || sEquation.Length == 0)
-            {
-                Console.WriteLine($"Give equation is {sEquation.Length} characters long, the maximum is {StringCalculator._maxLen}, please re enter the equation");
-                sEquation = Console.ReadLine();
-            }
+            (int code, string message) status = sc.Calc(Console.ReadLine());
+            while (status.code == -1) status = sc.Calc(Console.ReadLine());
 
-           
+
             // CALC
 
             Console.Read();
         }
 
+    }
 }
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -2,20 +2,25 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 
-class StringCalculator
+namespace Calculator
 {
 
-    static string sEquation;
-    private const string _sOperators = "*/%+-";
-    private const string _sDigits = "0123456789.";
-    private static List<double> iNumsInEq = new List<double>();
-    private static List<char> cOpsInEq = new List<char>();
-    public const int _maxLen = 20;
+    class StringCalculator
+    {
+
+        static string sEquation;
+        private const string _sOperators = "*/%+-";
+        private const string _sDigits = "0123456789.";
+        private static List<double> iNumsInEq = new List<double>();
+        private static List<char> cOpsInEq = new List<char>();
+        public const int _maxLen = 20;
 
 
-    public string Calc(string EquationStr) {
-        if (EquationStr.Length > _maxLen || EquationStr.Length < 1) return $"Give equation is {sEquation.Length} characters long, the maximum is {_maxLen}, please re enter the equation";
-        if (getEquation())
+        public (int, string) Calc(string EquationStr)
+        {
+            if (EquationStr.Length > _maxLen || EquationStr.Length < 1) return (-1, $"Give equation is {sEquation.Length} characters long, the maximum is {_maxLen}, please re enter the equation");
+            if (EquationStr == "help") return (1, getHelp());
+            if (getEquation())
             {
                 var copyOpsInEq = cOpsInEq.ToList();
                 foreach (char op in copyOpsInEq)
@@ -25,7 +30,7 @@ class StringCalculator
 
                 if (oofIndex('+') == -1 && oofIndex('-') == -1)
                 {
-                    return $"{sEquation}={iNumsInEq[0]}";
+                    return (0, $"{sEquation}={iNumsInEq[0]}");
                 }
                 else
                 {
@@ -36,127 +41,128 @@ class StringCalculator
                         answer = AddSub(answer, iNumsInEq[i], cOpsInEq[opc]);
                         opc++;
                     }
-                    return $"{sEquation}={answer}";
+                    return (0, $"{sEquation}={answer}");
                 }
             }
-        return "ERROR: Equation could not have been calculated";
-    }
-
-    public string getHelp() => 
-        "******************************\n" +
-        "** Help Sheet                *\n" +
-        "** Functions :               *\n" +
-        "** Addition: a+b             *\n" +
-        "** Subtraction: a-b          *\n" +
-        "** Multiplication: a*b       *\n" +
-        "** Division: a/b             *\n" +
-        "** Modulus: a%b              *\n" +
-        "** Brackets: a*(b+c)         *\n" +
-        "** This is the basic syntax  *\n" +
-        "** of the calculator, all the*\n" +
-        "** functions can be combined *\n" +
-        "******************************\n";
-
-    static double AddSub(double num1, double num2, char coperator)
-    {
-        double tmpans = 0;
-        switch (coperator)
-        {
-            case '+':
-                tmpans = num1 + num2;
-                break;
-            case '-':
-                tmpans = num1 - num2;
-                break;
-            default:
-                break;
+            return (-2, "ERROR: Equation could not have been calculated");
         }
-        return tmpans;
-    }
 
-    static void multiIndex(char op)
-    {
-        if (op == '*' || op == '/' || op == '%')
+        public string getHelp() =>
+            "******************************\n" +
+            "** Help Sheet                *\n" +
+            "** Functions :               *\n" +
+            "** Addition: a+b             *\n" +
+            "** Subtraction: a-b          *\n" +
+            "** Multiplication: a*b       *\n" +
+            "** Division: a/b             *\n" +
+            "** Modulus: a%b              *\n" +
+            "** Brackets: a*(b+c)         *\n" +
+            "** This is the basic syntax  *\n" +
+            "** of the calculator, all the*\n" +
+            "** functions can be combined *\n" +
+            "******************************\n";
+
+        static double AddSub(double num1, double num2, char coperator)
         {
-            int pos;
-            if ((pos = oofIndex(op)) > -1)
+            double tmpans = 0;
+            switch (coperator)
             {
-                fixList(pos, op);
+                case '+':
+                    tmpans = num1 + num2;
+                    break;
+                case '-':
+                    tmpans = num1 - num2;
+                    break;
+                default:
+                    break;
+            }
+            return tmpans;
+        }
+
+        static void multiIndex(char op)
+        {
+            if (op == '*' || op == '/' || op == '%')
+            {
+                int pos;
+                if ((pos = oofIndex(op)) > -1)
+                {
+                    fixList(pos, op);
+                }
             }
         }
-    }
 
-    static bool getEquation()
-    {
-        string stmp = "";
-
-        var cEquation = sEquation.ToCharArray();
-        for (int i = 0; i < sEquation.Length; i++)
+        static bool getEquation()
         {
-            if (_sDigits.Contains(cEquation[i].ToString()))
+            string stmp = "";
+
+            var cEquation = sEquation.ToCharArray();
+            for (int i = 0; i < sEquation.Length; i++)
             {
-                if (i == sEquation.Length - 1)
+                if (_sDigits.Contains(cEquation[i].ToString()))
                 {
-                    stmp += cEquation[i].ToString();
+                    if (i == sEquation.Length - 1)
+                    {
+                        stmp += cEquation[i].ToString();
+                        iNumsInEq.Add(Double.Parse(stmp));
+                        stmp = "";
+                    }
+                    else
+                    {
+                        stmp += (cEquation[i]).ToString();
+                    }
+                }
+                else if (_sOperators.Contains(cEquation[i].ToString()))
+                {
+                    cOpsInEq.Add(cEquation[i]);
                     iNumsInEq.Add(Double.Parse(stmp));
                     stmp = "";
                 }
                 else
                 {
-                    stmp += (cEquation[i]).ToString();
+                    Console.WriteLine($"{cEquation[i]} is not a digit or an operator");
+                    return false;
                 }
             }
-            else if (_sOperators.Contains(cEquation[i].ToString()))
-            {
-                cOpsInEq.Add(cEquation[i]);
-                iNumsInEq.Add(Double.Parse(stmp));
-                stmp = "";
-            }
-            else
-            {
-                Console.WriteLine($"{cEquation[i]} is not a digit or an operator");
-                return false;
-            }
+            return true;
         }
-        return true;
-    }
 
 
-    static int oofIndex(char op)
-    {
-        int index = -1;
-        for (int i = 0; i < cOpsInEq.Count; i++)
+        static int oofIndex(char op)
         {
-            if (op == cOpsInEq[i])
+            int index = -1;
+            for (int i = 0; i < cOpsInEq.Count; i++)
             {
-                index = i;
-                break;
+                if (op == cOpsInEq[i])
+                {
+                    index = i;
+                    break;
+                }
+
             }
-
+            return index;
         }
-        return index;
-    }
 
-    static void fixList(int opPos, char op)
-    {
-        double tmphigh = 0;
-        switch (op)
+        static void fixList(int opPos, char op)
         {
-            case '*':
-                tmphigh = iNumsInEq[opPos] * iNumsInEq[opPos + 1];
-                break;
-            case '/':
-                tmphigh = iNumsInEq[opPos] / iNumsInEq[opPos + 1];
-                break;
-            case '%':
-                tmphigh = iNumsInEq[opPos] % iNumsInEq[opPos + 1];
-                break;
-            default:
-                break;
+            double tmphigh = 0;
+            switch (op)
+            {
+                case '*':
+                    tmphigh = iNumsInEq[opPos] * iNumsInEq[opPos + 1];
+                    break;
+                case '/':
+                    tmphigh = iNumsInEq[opPos] / iNumsInEq[opPos + 1];
+                    break;
+                case '%':
+                    tmphigh = iNumsInEq[opPos] % iNumsInEq[opPos + 1];
+                    break;
+                default:
+                    break;
 
+            }
+            iNumsInEq[opPos] = tmphigh;
+            iNumsInEq.RemoveAt(opPos + 1);
+            cOpsInEq.RemoveAt(opPos);
         }
-        iNumsInEq[opPos] = tmphigh;
-        iNumsInEq.RemoveAt(opPos + 1);
-        cOpsInEq.RemoveAt(opPos);
     }
 }
